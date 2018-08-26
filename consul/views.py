55ad16dc0157ac4a807e8b401e0b35ce033d4c24
Taylor <tquinn2035@gmail.com>
2018-08-26 12:07:49 -0700

    started schedule

--- consul/views.py
+++ consul/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from datetime import datetime
-from .models import Counselor, CounselorCourse
+from .models import Counselor, CounselorCourse, Course
 
 # Create your views here.
 
