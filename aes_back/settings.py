3a9f3c57e72bfb8e17b7fe003bca52421024ce54
tquinn35 <tquinn2035@gmail.com>
2018-10-05 08:43:42 -0700

    added static

--- aes_back/settings.py
+++ aes_back/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
